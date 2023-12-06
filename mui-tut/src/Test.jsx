b047a2dcea310b99797f1bfa091c6ca31842b872
Shashank Naik <shashanknaik808@gmail.com>
2023-12-06 17:26:08 +0530

    Add AppBar with AccountCircleIcon and DownloadForOfflineIcon in Toolbar

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
 
 function Test() {
     return (
-        <div>Test</div>
+        <Box>
+            <AppBar>
+                <Toolbar>
+                    <IconButton onClick={() => alert("Icon Selected")}>
+                        {" "}
+                        <AccountCircleIcon />
+                    </IconButton>
+                    <IconButton>
+                        {" "}
+                        <DownloadForOfflineIcon color='inherit' />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
